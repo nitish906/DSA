77951511dee436a7ec3d09623ba0d960f8748492
nitish906 <nk08522015@gmail.com>
2023-02-19 22:45:53 +0530

    str01

--- String/src/com/masai/String/_10_PrintAllSubstring.java
+++ String/src/com/masai/String/_10_PrintAllSubstring.java
@@ -3,6 +3,18 @@ package com.masai.String;
 public class _10_PrintAllSubstring {
 	public static void findAllSubs(String string) {
 		
+		
+		int n=string.length();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				for(int k=i; k<=j; k++) {
+					System.out.print(string.charAt(k));
+				}
+				System.out.println();
+			}
+			
+		}
 	
 		
 	}
