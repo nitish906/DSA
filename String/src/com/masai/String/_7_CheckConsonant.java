73b017ea91d5d4fb1b9e22c06a59f24c62a66dda
nitish906 <nk08522015@gmail.com>
2023-02-20 12:19:13 +0530

    nk

--- String/src/com/masai/String/_7_CheckConsonant.java
+++ String/src/com/masai/String/_7_CheckConsonant.java
@@ -3,11 +3,29 @@ package com.masai.String;
 public class _7_CheckConsonant {
 	
 	public static void checkConsonant(String string) {
+		String bagString="";
+		
+		for(int i=0; i<string.length(); i++) {
+			if(string.charAt(i)=='a' ||string.charAt(i)=='e' ||string.charAt(i)=='i' ||
+					string.charAt(i)=='o'|| string.charAt(i)=='u') {
+				
+				//bagString+=string.charAt(i);
+				System.out.println(true);
+				break;
+			}
+			else {
+				System.out.println(false);
+				break;
+			}
+			
+		}
+		
+		
 		
 	}
 	
 	public static void main(String[] args) {
-		String string= "aeou";
+		String string= "ae";
 		
 		checkConsonant(string);
 	}
--- String/src/com/masai/String/_8_CountVowelCons.java
+++ String/src/com/masai/String/_8_CountVowelCons.java
@@ -4,6 +4,20 @@ public class _8_CountVowelCons {
 	
 	public static void countVandC(String string) {
 		
+		int vCount=0;
+		int cCount=0;
+		
+		for(int i=0; i<string.length();i++) {
+			if(string.charAt(i)=='a' ||string.charAt(i)=='e' ||string.charAt(i)=='i' ||
+					string.charAt(i)=='o'|| string.charAt(i)=='u') {
+				vCount++;
+			}
+			else {
+				cCount++;
+			}
+		}
+		System.out.println(vCount+" "+cCount);
+		
 	}
 	
 	
