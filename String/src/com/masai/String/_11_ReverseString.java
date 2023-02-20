31ecf02ec61097bcc033e9cba0a822de39f6f719
nitish906 <nk08522015@gmail.com>
2023-02-20 17:02:24 +0530

    nk

--- String/src/com/masai/String/_11_ReverseString.java
+++ String/src/com/masai/String/_11_ReverseString.java
@@ -1,8 +1,19 @@
 package com.masai.String;
 
 public class _11_ReverseString {
+	public static void reverWords(String[] str) {
+		
+	}
+	
 	public static void main (String[] args) {
-		String str="";
-		   
+		String[] str= {"Welcome to Coding Ninjas"};
+		   reverWords(str);
 	}
 }
+
+
+//Input format :
+//Ninjas Coding to Welcome
+
+//Output format :
+//Welcome to Coding Ninjas
--- String/src/com/masai/String/_9_ChckPalindrom.java
+++ String/src/com/masai/String/_9_ChckPalindrom.java
@@ -3,6 +3,18 @@ package com.masai.String;
 public class _9_ChckPalindrom {
 	public static void checkPalindrom(String string) {
 		
+		String bagString="";
+		
+		for(int i=string.length()-1; i>=0; i--) {
+			bagString+=string.charAt(i);
+		}
+		
+		if(bagString.equals(string)) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
 	}
 	
 	public static void main(String[] args) {
