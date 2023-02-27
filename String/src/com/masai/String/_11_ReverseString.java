f057d011d15b19766060413f185fd50c8e313de0
nitish906 <nk08522015@gmail.com>
2023-02-27 21:53:30 +0530

    masai

--- String/src/com/masai/String/_11_ReverseString.java
+++ String/src/com/masai/String/_11_ReverseString.java
@@ -1,15 +1,22 @@
 package com.masai.String;
 
 public class _11_ReverseString {
-	public static void reverWords(String[] str) {
-		int n=str.length;
+	public static void reverWords(String str) {
 		
+		int input=str.length();
+		String[] words = str.split(" ");
+		String outputStr = "";
+				for (int i = words.length - 1; i >= 0; i--) {
+					outputStr += words[i] + " ";
+				}
+
+			System.out.println(outputStr);	
 		
 		
 	}
 	
 	public static void main (String[] args) {
-		String[] str= {"Welcome to Masia School"};
+		String str= "Welcome to Masia School";
 		   reverWords(str);
 	}
 }
