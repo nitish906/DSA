8472f4184beedf7e7783cd6029d7e4464ef1b0f5
nitish906 <nk08522015@gmail.com>
2023-02-27 21:19:00 +0530

    masai

--- String/src/com/masai/String/_11_ReverseString.java
+++ String/src/com/masai/String/_11_ReverseString.java
@@ -4,11 +4,7 @@ public class _11_ReverseString {
 	public static void reverWords(String[] str) {
 		int n=str.length;
 		
-		String bag="";
-		for(int i=0; i<n; i++) {
-			bag+=str[i];
-		}
-		System.out.println(bag.length());
+		
 		
 	}
 	
