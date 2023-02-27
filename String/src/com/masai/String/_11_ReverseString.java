4be28197438af8d5b8fff78709ee9bb8e85b3f7c
nitish906 <nk08522015@gmail.com>
2023-02-27 21:17:25 +0530

    masai

--- String/src/com/masai/String/_11_ReverseString.java
+++ String/src/com/masai/String/_11_ReverseString.java
@@ -2,18 +2,25 @@ package com.masai.String;
 
 public class _11_ReverseString {
 	public static void reverWords(String[] str) {
+		int n=str.length;
+		
+		String bag="";
+		for(int i=0; i<n; i++) {
+			bag+=str[i];
+		}
+		System.out.println(bag.length());
 		
 	}
 	
 	public static void main (String[] args) {
-		String[] str= {"Welcome to Coding Ninjas"};
+		String[] str= {"Welcome to Masia School"};
 		   reverWords(str);
 	}
 }
 
 
-//Input format :
-//Ninjas Coding to Welcome
+
+
 
 //Output format :
-//Welcome to Coding Ninjas
+// Masia School to Welcome
--- String/src/com/masai/String/_17_CompressTheString.java
+++ String/src/com/masai/String/_17_CompressTheString.java
@@ -3,7 +3,7 @@ package com.masai.String;
 public class _17_CompressTheString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
